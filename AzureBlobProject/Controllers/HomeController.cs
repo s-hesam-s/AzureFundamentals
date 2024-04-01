dc0ca79253b03cee2cecdc65fb287e9c154ab274
s-hesam-s <hesamshoushtarian@gmail.com>
2024-04-01 16:28:31 +0330

    Section 2 - Display Container Blobs and Account

--- AzureBlobProject/Controllers/HomeController.cs
+++ AzureBlobProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using AzureBlobProject.Models;
+using AzureBlobProject.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +7,15 @@ namespace AzureBlobProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IContainerService _containerService;
+        public HomeController(IContainerService containerService)
         {
-            _logger = logger;
+            _containerService = containerService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _containerService.GetAllContainerAndBlobs());
         }
 
         public IActionResult Privacy()
