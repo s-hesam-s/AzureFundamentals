bbc58c24412d561f9dde224c1c15e94e754b8d31
s-hesam-s <hesamshoushtarian@gmail.com>
2024-04-01 16:05:49 +0330

    Section 2 - Upload Blob

--- AzureBlobProject/Controllers/BlobController.cs
+++ AzureBlobProject/Controllers/BlobController.cs
@@ -17,5 +17,28 @@ namespace AzureBlobProject.Controllers
             var blobsObj = await _blobService.GetAllBlobs(containerName);
             return View(blobsObj);
         }
+
+        [HttpGet]
+        public IActionResult AddFile(string containerName)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddFile(string containerName,IFormFile file)
+        {
+            if (file == null || file.Length < 1) return View();
+
+            //file name - xps_img2.png 
+            //new name - xps_img2_GUIDHERE.png
+            var fileName = Path.GetFileNameWithoutExtension(file.FileName) + "_" + Guid.NewGuid() + Path.GetExtension(file.FileName);
+
+            var result = await _blobService.UploadBlob(fileName, file, containerName);
+
+            if (result)
+                return RedirectToAction("Index", "Container");
+
+            return View();
+        }
     }
 }
