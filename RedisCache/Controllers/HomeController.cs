c18ff286d40993cd6e1f309ce8ad1472bfdd3a4a
s-hesam-s <hesamshoushtarian@gmail.com>
2024-04-10 18:08:28 +0330

    Section 9 - Invalidate Azure Cache

--- RedisCache/Controllers/HomeController.cs
+++ RedisCache/Controllers/HomeController.cs
@@ -34,7 +34,11 @@ namespace RedisCache.Controllers
             else
             {
                 categoryList = _db.Categories.ToList();
-                _cache.SetString("categoryList", JsonConvert.SerializeObject(categoryList));
+
+                DistributedCacheEntryOptions options = new();
+                options.SetAbsoluteExpiration(new TimeSpan(0, 0, 30));
+
+                _cache.SetString("categoryList", JsonConvert.SerializeObject(categoryList), options);
             }
             return View(categoryList);
         }
